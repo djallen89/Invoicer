9e0c0d622ac3e7057252727d47545e9278aa85c9
Dominick Allen <dominick.allen1989@gmail.com>
2019-09-14 22:06:21 -0400

    Working on PDF representation

--- pdfbuilder.h
+++ pdfbuilder.h
@@ -2,6 +2,7 @@
 #define PDFBUILDER_H
 
 #include <QString>
+#include <QVector>
 
 namespace PDF {
     enum class DocumentClass {
@@ -9,18 +10,35 @@ namespace PDF {
         letter
     };
 
-    
+    enum class textSize {
+        Huge,
+        huge,
+        LARGE,
+        Large,
+        large,
+        normalsize,
+        small,
+        footnotesize,
+        scriptsize,
+        tiny
+    };
 }
 
 class PDFBuilder
 {
 public:
     PDFBuilder();
-    //QString simple_output();
+    QString simple_output();
     ~PDFBuilder();
 
 private:
-    
+    PDF::DocumentClass docClass;
+    QString *preamble;
+    QString *biller_info;
+    QString *client_info;
+    QString *invoice_number;
+    QVector<QString*> line_items;
+    void build_preamble();
 };
 
 #endif // PDFBUILDER_H
